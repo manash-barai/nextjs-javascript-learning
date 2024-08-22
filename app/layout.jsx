272e5c0c99b60ffff1e59afdae9f31c15f137bd7
manash-barai <manashjbarai@gmail.com>
2024-08-23 02:56:34 +0530

    title change to crud

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "curd",
+  title: "crud",
   description: "Discover & Share AI Prompts",
 };
 
